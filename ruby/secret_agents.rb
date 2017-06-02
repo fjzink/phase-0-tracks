b6f705c57b97ebb7d8aa9fe1b5b94a6a8432a058
Fulton Zink <joeyzink@gmail.com>
2017-06-01 19:23:58 -0700

    Add decrypt method and debug encrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,16 +1,42 @@
-#Use for loop to reassign each index in the string using .next method
-#Run for loop on length of password 
-#Use if statement to check if the index is a space or not
-#Print reassigned password
+#Use for loop to reassign each index in the string using .next method.
+#Run for loop on length of password .
+#Use if statement to check if the index is a space or not.
+#Print reassigned password.
 
 def encrypt(password)
 	for i in 0...password.length
 		if password[i] == " "
 		else
-			password[i] = password[i].next
+			if password[i] == "z"
+				password[i] = "a"
+			else
+				password[i] = password[i].next
+			end
 		end
 	end
 	p password
 end
 
 encrypt("password password")
+
+#Use for loop to cycle through each letter of the encrypted password.
+#Get the index of each character from the password in the alphabet.
+#Use if statement to check if the character is a space or not.
+#Subtract 1 from the alphabet index and use that to reassign each letter
+#in the password using the alphabet string.
+
+def decrypt(pass)
+	for i in 0...pass.length
+		if pass[i] == " "
+		else
+			ind = "abcdefghijklmnopqrstuvwxyz".index(pass[i])
+			pass[i] = "abcdefghijklmnopqrstuvwxyz"[ind-1]
+		end
+	end
+	p pass
+end
+
+encrypt("abc")
+encrypt("zed")
+decrypt("bcd")
+decrypt("afe")
