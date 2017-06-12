798b8947e17a854d20a10f07413aecb597c0bb08
Fulton Zink <joeyzink@gmail.com>
2017-06-12 15:23:09 -0700

    Add release 1 and 2

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -14,3 +14,27 @@ Puppy:
 	Nose boops
 =end
 
+=begin
+class Puppy
+	end
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
+
+p spot.class
+p duchess == fido
+p fido.instance_of?(Array)
+p fido.instance_of?(Puppy)
+p spot.play_dead
+=end
+
+=begin
+str = String.new
+p str.length
+p str.empty?
+p str += "huh."
+greeting = String.new("hello")
+p greeting.length
+p greeting.upcase
+=end
