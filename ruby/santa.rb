5afa5dcd997361a34c8e9ccb26a6962aed1a1512
Fulton Zink <joeyzink@gmail.com>
2017-06-12 22:01:34 -0700

    Add Santa class

---
+++ ruby/santa.rb
@@ -0,0 +1,17 @@
+class Santa
+	def speak()
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!"
+	end
+
+	def initialize
+		puts "Initializing Santa instance ..."
+	end
+end
+
+chocolate_chip = Santa.new
+speak
+eat_milk_and_cookies("Snickerdoodle")
