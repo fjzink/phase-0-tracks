863e4c68120e6d390b98cd1fbc0615198542c179
Fulton Zink <joeyzink@gmail.com>
2017-06-13 21:22:12 -0700

    Add getter and setter methods for release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -18,6 +18,33 @@ class Santa
 	def about
 		puts "This Santa is #{@gender} and #{@ethnicity}."
 	end
+
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at(name)
+		ind = @reindeer_ranking.index(name)
+		@reindeer_ranking[ind], @reindeer_ranking[-1] = @reindeer_ranking[-1], @reindeer_ranking[ind]
+	end
+
+	#setter
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
+	#getter
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+	def reindeer_ranking
+		@reindeer_ranking
+	end
 end
 
 jolly_santa = Santa.new("Dude", "Santa")
@@ -33,3 +60,11 @@ for i in 0...genders.length
 end
 
 santas.each {|x| x.about}
+
+jolly_santa.celebrate_birthday
+p jolly_santa.age
+jolly_santa.get_mad_at("Vixen")
+p jolly_santa.reindeer_ranking
+jolly_santa.gender=("N/A")
+jolly_santa.about
+p jolly_santa.ethnicity
