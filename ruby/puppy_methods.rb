a463d349018a5db280f7c70b81e1ec791e5d1c98
Fulton Zink <joeyzink@gmail.com>
2017-06-14 11:20:17 -0700

    Add methods for release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,28 @@ class Puppy
     toy
   end
 
+  def speak(int)
+	int.times do 
+		puts "Woof!"
+	end
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(int)
+  	int * 7
+  end
+
+  def play_dead
+  	puts "*plays dead*"
+  end
 end
 
+puppy = Puppy.new
+puppy.fetch("bone")
+puppy.speak(5)
+puppy.roll_over
+puppy.dog_years(2)
+puppy.play_dead
