c128ced472581d168c46cd6b29d824832ff1fc72
Fulton Zink <joeyzink@gmail.com>
2017-06-21 13:49:51 -0700

    Add explore.js with pseudocode

---
+++ js/explore.js
@@ -0,0 +1,10 @@
+/*
+function: reverse(str)
+input: string
+output: string
+*Transform the input string into a an array of the characters from the string.
+*Iterate through this array and add each index to the beginning of a new array.
+*Transform the second array back into a string.
+*Return the new string.
+*/
+
