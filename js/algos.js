dac64207545595b4b956e371eedee619278f1363
Fulton Zink <joeyzink@gmail.com>
2017-06-22 16:18:53 -0700

    Add release 1 with function to check for matching key-value pairs in two objects

--- js/algos.js
+++ js/algos.js
@@ -10,12 +10,39 @@ output: string
 function longestString(arr) {
 	var biglyString = "";
 	for (var i = 0; i < arr.length; i++) {
-		if (arr[i].length > biglyString) {
+		if (arr[i].length > biglyString.length) {
 			biglyString = arr[i];
 		}
 	}
 	return biglyString
 }
 
-someStrings = ["a string", "a bigger string", "the biggest string"]
-console.log(longestString(someStrings))
+someStrings = ["a string", "a bigger string", "the biggest string"];
+console.log(longestString(someStrings));
+
+/*
+Release 1
+input: object, object
+output: boolean
+*Intialize a variable as false.
+*Loop through the keys in each array.
+*If two key-value pairs are equal, then set the variable to true.
+*Return the variable with boolean value stored to it.
+*/
+
+function matchingPair(objFirst, objSecond) {
+	var match = false;
+	for (var keyFirst in objFirst) {
+		for (var keySecond in objFirst) {
+			if ((keyFirst === keySecond) && (objFirst[keyFirst] === objSecond[keySecond])) {
+				match = true;
+			}
+		}
+	}
+	return match
+}
+
+var objOne = {aKey: "a value", anotherKey: "another value"};
+var objTwo = {someKey: "some value", aKey: "a value"};
+
+console.log(matchingPair(objOne, objTwo))
