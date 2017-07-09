b2bfd3d01e79fc0346c630b2a378d31177685a57
Fulton Zink <joeyzink@gmail.com>
2017-07-09 15:00:25 -0700

    Add database and table. Add basic structure for user interface.

--- databases/wk8-solo/todolist.rb
+++ databases/wk8-solo/todolist.rb
@@ -2,3 +2,44 @@ require 'sqlite3'
 
 #Create the to do list
 to_do_list = Database.new("todolist.db")
+
+make_table = <<-SQL
+	CREATE TABLE IF NOT EXISTS todolist (
+	id INTEGER PRIMARY KEY,
+	item VARCHAR(255),
+	month INT,
+	day INT,
+	year INT
+	)
+SQL
+
+to_do_list.execute(make_table)
+
+#User interface
+
+puts "Welcome to your to do list!"
+puts "Type \'done\' to exit the program."
+
+done = false
+while !done
+	puts "Enter command:"
+	input = gets.chomp
+
+	#Execute a command
+	if input == "add"
+
+	elsif input = "delete"
+
+	elsif input = "update"
+
+	elsif input = "show"
+
+	else
+		puts "Invalid input."
+	end
+
+	#End loop if user types "done"
+	if input == "done"
+		done = true
+	end
+end
